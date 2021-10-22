0217d09c98693f3c2475b393ca8236d9d9605070
JINJIALEI <jinjialei@JINJIALEIdeMacBook-Pro.local>
2021-10-22 20:22:58 +0800

    composer update

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $a = '506,$63.85,270,$59.48,62,$30.47,366,$44.74,12,$41.69,772,$4.86,1494,$8.43,480,$12.00,64,$15.57,22,$19.14,';
-    $res = explode(' ', $a);
-
-    $filter1 = array_filter($res,function($var){
-        return(!($var & 1));
-    },ARRAY_FILTER_USE_KEY);
-
-    $filter2 = array_filter($res,function($var){
-        return($var & 1);
-    },ARRAY_FILTER_USE_KEY);
-
-    foreach ($filter1 as $item){
-        $item = str_replace(',', '', $item);
-        echo ($item/2).'<br/>';
-        echo ($item/2).'<br/>';
-    }
-echo "<br/><br/><br/>";
-die();
-    return view('welcome', compact('filter1', 'filter2'));
+    return view('welcome');
 });
 
 
